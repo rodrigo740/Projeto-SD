eef97aa9f2cbc504dc6f037974058317fd663f81
rodrigo740 <rodrigomartins@ua.pt>
2022-04-08 15:23:37 +0100

    MemFIFO and MemObject added, Table class innit

--- src/commInfra/Fifo_abstract1.java
+++
@@ -1,8 +0,0 @@
-package commInfra;
-
-abstract class Fifo_abstract1<T> {
-    public abstract void add(Object letter);
-    public abstract Object remove();
-    public abstract Object size();
-    public abstract void insert();
-}
--- src/sharedRegion/Table.java
+++ src/sharedRegion/Table.java
@@ -1,5 +1,43 @@
 package sharedRegion;
 
+import commInfra.MemFIFO;
+import entities.Student;
+import genclass.GenericIO;
+
 public class Table {
+	
+	private final int sitsNumber;
+	
+	private boolean organizingOrder;
+	private boolean allHaveChosen;
+	private boolean orderDone;
+	
+	private MemFIFO<Integer> sitOrder;
+	private MemFIFO<Integer> eatOrder;
+	
+	private final Student[] students;
+	private final GeneralRepo repos;
+	
+	public Table(GeneralRepo repos, int sitsNumber) {
+		this.sitsNumber = sitsNumber;
+		students = new Student[this.sitsNumber];
+		for (int i = 0; i < students.length; i++) {
+			students[i] = null;
+		}
+		
+		try {
+			sitOrder = new MemFIFO<> (new Integer [sitsNumber]);
+			eatOrder = new MemFIFO<> (new Integer [sitsNumber]);
+		} catch (Exception e) {
+			GenericIO.writelnString ("Instantiation of waiting FIFO failed: " + e.getMessage ());
+			sitOrder = null;
+			eatOrder = null;
+			System.exit(1);
+		}
+
+		this.repos = repos;
+	}
+	
+	
 
 }
