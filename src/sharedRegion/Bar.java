74e091e63e2cbc391d3e055db97ddcdd405e7d98
rodrigo740 <rodrigomartins@ua.pt>
2022-04-08 15:54:18 +0100

    bar getters and setters

--- src/sharedRegion/Bar.java
+++ src/sharedRegion/Bar.java
@@ -9,11 +9,11 @@ public class Bar {
 	private final Waiter waiter;
 	private final GeneralRepo repos;
 	
-	private boolean hasCalledWaiter;
 	private boolean wantsToPay;
 	private boolean describedOrder;
 	private boolean signalWaiter;
 	private boolean billHonored;
+	private boolean hasCalledWaiter;
 	
 	
 	public Bar(GeneralRepo repos) {
@@ -22,7 +22,52 @@ public class Bar {
 		this.repos = repos;
 	}
 	
+	public boolean isHasCalledWaiter() {
+		return hasCalledWaiter;
+	}
+
+	public void setHasCalledWaiter(boolean hasCalledWaiter) {
+		this.hasCalledWaiter = hasCalledWaiter;
+	}
+
+
+	public boolean isWantsToPay() {
+		return wantsToPay;
+	}
+
+
+	public void setWantsToPay(boolean wantsToPay) {
+		this.wantsToPay = wantsToPay;
+	}
+
+
+	public boolean isDescribedOrder() {
+		return describedOrder;
+	}
+
+
+	public void setDescribedOrder(boolean describedOrder) {
+		this.describedOrder = describedOrder;
+	}
+
+
+	public boolean isSignalWaiter() {
+		return signalWaiter;
+	}
+
+
+	public void setSignalWaiter(boolean signalWaiter) {
+		this.signalWaiter = signalWaiter;
+	}
+
+
+	public boolean isBillHonored() {
+		return billHonored;
+	}
+
+
+	public void setBillHonored(boolean billHonored) {
+		this.billHonored = billHonored;
+	}
 	
-
-
 }
