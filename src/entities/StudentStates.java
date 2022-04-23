7bd7c310e4cb76649b221f2ae268a8b9118661c3
raquel.milheiro2000@gmail.com <raquel.milheiro2000@gmail.com>
2022-04-23 12:26:53 +0100

    some comments

--- src/entities/StudentStates.java
+++ src/entities/StudentStates.java
@@ -1,16 +1,47 @@
 package entities;
 
+/**
+ * Definition of the internal states of the student during his life cycle.
+ */
+
 public class StudentStates {
 
-	public static final int GGTRT = 0; // going to the restaurant
-	public static final int TKSTT = 1; // taking a seat at the table
-	public static final int SELCS = 2; // selecting the courses
-	public static final int OGODR = 3; // organizing the order
-	public static final int CHTWC = 4; // chatting with companions
-	public static final int EJYML = 5; // enjoying the meal
-	public static final int PYTBL = 6; // paying the bill
-	public static final int GGHOM = 7; // going home
+	/**
+	 * going to the restaurant
+	 */
+	public static final int GGTRT = 0;
+	/**
+	 * taking a seat at the table
+	 */
+	public static final int TKSTT = 1;
+	/**
+	 * selecting the courses
+	 */
+	public static final int SELCS = 2;
+	/**
+	 * organizing the order
+	 */
+	public static final int OGODR = 3;
+	/**
+	 * chatting with companions
+	 */
+	public static final int CHTWC = 4;
+	/**
+	 * enjoying the meal
+	 */
+	public static final int EJYML = 5;
+	/**
+	 * paying the bill
+	 */
+	public static final int PYTBL = 6;
+	/**
+	 * going home
+	 */
+	public static final int GGHOM = 7;
 
+	/**
+	 * It can't be instantiated.
+	 */
 	private StudentStates() {
 	}
 
--- src/entities/WaiterStates.java
+++ src/entities/WaiterStates.java
@@ -1,15 +1,43 @@
 package entities;
 
+/**
+ * Definition of the internal states of the waiter during his life cycle.
+ */
+
 public class WaiterStates {
 
-	public static final int APPST = 0; // appraising situation
-	public static final int PRSMN = 1; // presenting the menu
-	public static final int TKODR = 2; // taking the order
-	public static final int PCODR = 3; // placing the order
-	public static final int WTFPT = 4; // waiting for portion
-	public static final int PRCBL = 5; // processing the bill
-	public static final int RECPM = 6; // receiving payment
+	/**
+	 * appraising situation
+	 */
+	public static final int APPST = 0;
+	/**
+	 * presenting the menu
+	 */
+	public static final int PRSMN = 1;
+	/**
+	 * taking the order
+	 */
+	public static final int TKODR = 2;
+	/**
+	 * placing the order
+	 */
+	public static final int PCODR = 3;
+	/**
+	 * waiting for portion
+	 */
+	public static final int WTFPT = 4;
+	/**
+	 * processing the bill
+	 */
+	public static final int PRCBL = 5;
+	/**
+	 * receiving payment
+	 */
+	public static final int RECPM = 6;
 
+	/**
+	 * It can't be instantiated.
+	 */
 	private WaiterStates() {
 	}
 
